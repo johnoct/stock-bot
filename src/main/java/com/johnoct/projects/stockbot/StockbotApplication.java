0f5706f33a955dbb6b9a5d5d1c3f3619f73370c7
John Octubre <johnoctubre7@gmail.com>
2018-01-21 15:20:56 -0500

    Initial Commit

---
+++ src/main/java/com/johnoct/projects/stockbot/StockbotApplication.java
@@ -0,0 +1,12 @@
+package com.johnoct.projects.stockbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockbotApplication.class, args);
+	}
+}
